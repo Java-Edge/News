7ca73a407ec03bdfe700b31ccfc2024d38148f2e
Wasabi1234 <2855845850@qq.com>
2017-05-10 11:36:39 +0800

    注册登录

--- src/main/java/com/sss/controller/LoginController.java
+++ src/main/java/com/sss/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.sss.model.News;
 import com.sss.model.ViewObject;
 import com.sss.service.NewsService;
 import com.sss.service.UserService;
+import com.sss.util.ToutiaoUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.jws.WebParam;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Shusheng Shi on 2017/5/8.-1
@@ -30,13 +34,71 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+    //    此函数直接调用各种service即可
+
+    /**
+     * 注册功能
+     * @param model:模型
+     * @param username:用户名
+     * @param password:密码
+     * @param rememberme:记住密码
+     * @return
+     */
     @RequestMapping(path = "/reg/", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String reg(Model model, @RequestParam("username") String username,
                       @RequestParam("password") String password,
                       @RequestParam(value = "rember", defaultValue = "0") int rememberme) {
 
+
+        try {
+            Map<String, Object> map = userService.register(username, password);
+            if (map.isEmpty()) {
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            } else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+
+        } catch (Exception e) {
+//          注册异常时,须记录在日志内
+            logger.error("注册异常" + e.getMessage());
+            return ToutiaoUtil.getJSONString(1, "注册异常");
+        }
+
     }
 
+    //    此函数直接调用各种service即可
+
+    /**
+     * 登录功能
+     * @param model
+     * @param username
+     * @param password
+     * @param rememberme
+     * @return
+     */
+    @RequestMapping(path = "/login/", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String login(Model model, @RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      @RequestParam(value = "rember", defaultValue = "0") int rememberme) {
+
+
+        try {
+            Map<String, Object> map = userService.register(username, password);
+            if (map.isEmpty()) {
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            } else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+
+        } catch (Exception e) {
+//          注册异常时,须记录在日志内
+            logger.error("注册异常" + e.getMessage());
+            return ToutiaoUtil.getJSONString(1, "注册异常");
+        }
+
+    }
 
 
 }
--- src/main/java/com/sss/service/UserService.java
+++ src/main/java/com/sss/service/UserService.java
@@ -2,12 +2,15 @@ package com.sss.service;
 
 import com.sss.dao.UserDAO;
 import com.sss.model.User;
+import com.sss.util.ToutiaoUtil;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by Shusheng Shi on 2017/5/7-4
@@ -28,16 +31,70 @@ public class UserService {
         Map<String, Object> map = new HashMap<>();
         if (StringUtils.isBlank(username)) {
             map.put("msgname", "用户名不能为空");
+            return map;
         }
 
         if (StringUtils.isBlank(password)) {
             map.put("msgpassword", password);
+            return map;
         }
 
         User user = userDAO.selectByName(username);
         if (user != null) {
-            
+            map.put("msgname", "此用户名已经被注册");
+            return map;
         }
+
+//      密码强度
+        user = new User();
+        user.setName(username);
+
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
+        String head = String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000));
+        user.setHeadUrl(head);
+        user.setPassword(ToutiaoUtil.MD5(password + user.getSalt()));
+        //直接放入密码,则是明文保存,这是绝对禁止的!!!
+//      user.setPassword(password);
+        userDAO.addUser(user);
+        return map;
+
+
+    }
+
+    /**
+     * 登录
+     * @param username:用户名
+     * @param password:密码
+     * @return
+     */
+    public Map<String,Object> login(String username, String password) {
+        Map<String, Object> map = new HashMap<>();
+        if (StringUtils.isBlank(username)) {
+            map.put("msgname", "用户名不能为空");
+            return map;
+        }
+
+        if (StringUtils.isBlank(password)) {
+            map.put("msgpassword", "密码不能为空");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+        if (user == null) {
+            map.put("msgname", "用户名不存在");
+            return map;
+        }
+
+
+//      判断用户存在后,开始验证密码
+        if (!ToutiaoUtil.MD5(password + user.getSalt()).equals(user.getPassword())) {
+            map.put("msgpwd", "密码不正确");
+            return map;
+        }
+
+//      用户登录成功
+//      给用户下发ticket
+        return map;
 
 
     }
